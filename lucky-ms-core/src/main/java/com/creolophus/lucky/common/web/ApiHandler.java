6f8b83b3c8b4bcbece99a5dca50e86201fa418e8
jinsong <jinsong@bjjctp.cn>
2021-12-15 15:40:37 +0800

    apiHandler 打印日志

--- lucky-ms-core/src/main/java/com/creolophus/lucky/common/web/ApiHandler.java
+++ lucky-ms-core/src/main/java/com/creolophus/lucky/common/web/ApiHandler.java
@@ -3,6 +3,8 @@ package com.creolophus.lucky.common.web;
 import com.creolophus.lucky.common.exception.HttpStatusException;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -10,6 +12,8 @@ import org.springframework.http.HttpStatus;
  * @date 2021/9/4 17:02
  */
 public class ApiHandler {
+  private static final Logger logger = LoggerFactory.getLogger(ApiHandler.class);
+
 
   public void handle(Api api, HttpServletRequest request) {
     handleScope(api,request);
@@ -18,6 +22,11 @@ public class ApiHandler {
   protected void handleScope(Api api, HttpServletRequest request) {
     if(api.scope().equals(Api.SCOPE_INTERNAL)){
       String header = request.getHeader(Api.INTERNAL_HEADER_KEY);
+
+      if (logger.isDebugEnabled()) {
+        logger.debug("{} {} {}", Api.SCOPE_INTERNAL, Api.INTERNAL_HEADER_KEY,header);
+      }
+
       if(StringUtils.isBlank(header) || !header.equals(Api.INTERNAL_HEADER_VAL)){
         throw new HttpStatusException(HttpStatus.UNAUTHORIZED);
       }
